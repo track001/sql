cae2052b86ac305e9cf8bf94524cf631db750ee7
track001 <45890450+track001@users.noreply.github.com>
2022-07-20 09:40:47 -0600

    Create Restricted Grouped Resuts with HAVING.sql

---
+++ Restricted Grouped Resuts with HAVING.sql
@@ -0,0 +1,55 @@
+CREATE TABLE exercise_logs
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT,
+    minutes INTEGER, 
+    calories INTEGER,
+    heart_rate INTEGER);
+
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("biking", 30, 115, 110);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("biking", 10, 45, 105);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("dancing", 15, 200, 120);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("dancing", 15, 165, 120);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("tree climbing", 30, 70, 90);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("tree climbing", 25, 72, 80);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("rowing", 30, 70, 90);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("hiking", 60, 80, 85);
+
+/*
+DATABASE SCHEMA
+exercise_logs
+        8 rows
+id (PK)INTEGER
+typeTEXT
+minutesINTEGER
+caloriesINTEGER
+heart_rateINTEGER
+*/
+
+SELECT * FROM exercise_logs;
+
+SELECT type, SUM(calories) AS total_calories FROM exercise_logs GROUP BY type;
+
+SELECT type, SUM(calories) AS total_calories FROM exercise_logs
+    GROUP BY type
+    HAVING total_calories > 150
+    ;
+
+SELECT type, AVG(calories) AS avg_calories FROM exercise_logs
+    GROUP BY type
+    HAVING avg_calories > 70
+    ; 
+    
+SELECT type FROM exercise_logs GROUP BY type HAVING COUNT(*) >= 2;
+
+ /* QUERY RESULTS
+id	type	minutes	calories	heart_rate
+1	biking	30	100	110
+2	biking	10	30	105
+3	dancing	15	200	120
+4	dancing	15	165	120
+5	tree climbing	30	70	90
+6	tree climbing	25	72	80
+7	rowing	30	70	90
+8	hiking	60	80	85
+*/
+
