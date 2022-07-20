0653ee655862baf0726780b6453a4bc8d32e899d
track001 <45890450+track001@users.noreply.github.com>
2022-07-20 10:10:13 -0600

    Create Challenge: Gradebook.sql

---
+++ Challenge: Gradebook.sql
@@ -0,0 +1,28 @@
+/* Gradebook database */
+CREATE TABLE student_grades (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    number_grade INTEGER,
+    fraction_completed REAL);
+    
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winston", 90, 0.805);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winnefer", 95, 0.901);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winsteen", 85, 0.906);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Wincifer", 66, 0.7054);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winster", 76, 0.5013);
+INSERT INTO student_grades (name, number_grade, fraction_completed)
+    VALUES ("Winstonia", 82, 0.9045);
+    
+/*    
+DATABASE SCHEMA
+student_grades6 rows
+id (PK)INTEGER
+nameTEXT
+number_gradeINTEGER
+fraction_completedREAL
+*/
