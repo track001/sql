4e8b5c4d62efeece2cd858728dd2ba2da3833c5c
track001 <45890450+track001@users.noreply.github.com>
2022-07-20 08:55:06 -0600

    Update and rename Create Table & Insert Data to Create Table & Insert Data.sql

--- Create Table & Insert Data.sql
+++ Create Table & Insert Data.sql
@@ -1,8 +1,8 @@
-/** Grocery list:
+/* Grocery list:
 Bananas (4)
 Peanut Butter (1)
 Dark Chocolate Bars (2)
-**/
+*/
 
 CREATE TABLE groceries (id INTEGER PRIMARY KEY, 
 name TEXT, quantity INTEGER);
@@ -10,4 +10,5 @@ name TEXT, quantity INTEGER);
 INSERT INTO groceries VALUES (1, "Bananas", 4);
 INSERT INTO groceries VALUES (2, "Peanut Butter", 1);
 INSERT INTO groceries VALUES (3, "Dark Chocolate Bars", 2);
+
 SELECT * FROM groceries;
