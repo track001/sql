999fcb14e8c834b3aea9b88cf47c062d08ea5dc3
track001 <45890450+track001@users.noreply.github.com>
2022-07-20 09:06:35 -0600

    Create Challenge: TODO List Database Stats.sql

---
+++ Challenge: TODO List Database Stats.sql
@@ -0,0 +1,27 @@
+/* TODO List with a number of minutes it will take to complete each item.*/
+
+CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
+INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
+INSERT INTO todo_list VALUES (2, "vacuuming", 20);
+INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
+
+/* Insert another item into the to-do list with the estimated minutes it will take */
+/* 40 minutes is a joke */
+INSERT INTO todo_list VALUES (4, "Finish typing this challenge solution", 40);
+
+/*
+DATABASE SCHEMA
+___________________
+to do list      3 rows
+Id  (PK)          INTEGER
+item             TEXT
+minutes       INTEGER
+
+QUERY RESULTS
+aisle	SUM(quantity)
+____________________
+2	9
+4	1
+7	56
+12	1
+*/
